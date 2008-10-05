52084463f474e34864016efe9b7c7e904c56d8aa
Elan Ruusamäe <glen@pld-linux.org>
2008-10-05 12:19:27 +0000

    - rewrite script to handle multiple file imports - proper exit codes and error handling
    Changed files:
        cacti-add_template.php -> 1.2

--- cacti-add_template.php
+++ cacti-add_template.php
@@ -11,25 +11,23 @@ include_once("/usr/share/cacti/include/global.php");
 include_once("/usr/share/cacti/lib/import.php"); 
 
 // check if we have good number of argument
-if ($argc != 2 || in_array($argv[1], array('--help', '-help', '-h', '-?')))
-{
-  echo "usage : $argv[0] file.xml\n";
-  echo "this script will import file.xml template into cacti\n";
-  return 0;
+if ($argc < 2 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
+	echo "Usage: $argv[0] FILE.xml [FILE.xml]\n";
+	echo "This script will import FILE.xml template into cacti\n";
+	exit(0);
+}
+array_shift($argv);
+
+foreach ($argv as $xml_file) {
+	$xml_data = file_get_contents($xml_file);
+	if ($xml_data === false) {
+		echo "ERROR: cannot open $xml_file, exiting\n";
+		exit(1);
+	}
+
+	// import data into cacti. $info_array will contain debugging info.
+	echo "cacti-add_template: importing $xml_file\n";
+	$info_array = import_xml_data($xml_data, false);
 }
 
-// open the .xml file
-$xml_file = $argv[1];
-$fp = fopen($xml_file, "r");
-if ($fp == FALSE) 
-{
-  echo "cannot open $xml_file, exiting\n";
-  return -1;
-}
-$xml_data = fread($fp,filesize($xml_file));
-
-// import data into cacti. $info_array will contain debugging info.
-$info_array = import_xml_data($xml_data,false);
-
-return 0;
-?>
+exit(0);
